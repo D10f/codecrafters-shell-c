416fadcf9d6d4b3c390bccd6cff53c5becb81cf9
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-12-17 01:15:39 +0100

    [any message]

--- app/main.c
+++ app/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  printf("$ ");
-  fflush(stdout);
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+
+    // Remove newline from fgets
+    input[strcspn(input, "\n")] = '\0';
+    printf("%s: command not found\n", input);
+    return 0;
 }
