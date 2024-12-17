a8894996b01d912d9f2b09988a8b5ad54426fce8
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-12-17 01:19:13 +0100

    Testing REPL

--- app/main.c
+++ app/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 int main() {
-    printf("$ ");
-    fflush(stdout);
 
-    // Wait for user input
-    char input[100];
-    fgets(input, 100, stdin);
+    while ( true )
+    {
+        printf("$ ");
+        fflush(stdout);
 
-    // Remove newline from fgets
-    input[strcspn(input, "\n")] = '\0';
-    printf("%s: command not found\n", input);
+        // Wait for user input
+        char input[100];
+        fgets(input, 100, stdin);
+
+        // Remove newline from fgets
+        input[strcspn(input, "\n")] = '\0';
+        printf("%s: command not found\n", input);
+    }
+
     return 0;
 }
